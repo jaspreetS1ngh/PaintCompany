8a5f8c074b853b5f13de2f3d5002988ea95011f6
Jaspreet <js724415@gmail.com>
2024-03-24 14:21:59 -0700

    patchApi1

--- routes/api.js
+++ routes/api.js
@@ -23,6 +23,31 @@ router.get('/inventory', async (req, res) => {
       res.status(500).json('Error fetching inventory data By Id.');
     }
   });
+  router.patch('/inventory/:id', async (req, res) => {
+    
+
+    try {
+      console.log(req.body);
+      const { id } = req.params;
+    const inventoryDetails = req.body;
+        controllers.updateInventoryItem(id, inventoryDetails);
+        res.status(200);
+    } catch (error) {
+        console.error(`Error updating inventory item by ID: ${error}`);
+        res.status(500).json('Error updating inventory item by ID.');
+    }
+});
+  router.get('/staff/:id', async (req, res) => {
+    const {id}= req.params;
+    try {
+      const staffById = await controllers.getStaffDataById(id);
+      
+      res.status(200).json(staffById);
+    } catch (error) {
+      console.error(`Error fetching saff data By Id: ${error}`);
+      res.status(500).json('Error fetching staff data By Id.');
+    }
+  });
   router.get('/staff', async (req, res) => {
     try {
       const stockData = await controllers.getStaffData();
@@ -32,5 +57,7 @@ router.get('/inventory', async (req, res) => {
       res.status(500).json('Error fetching stock data.');
     }
   });
+  
+  
 
   module.exports=router;
